6b54e6236dd8566d5d8b7cbaebad58b6e7e76d9c
smdhz <msmdhz@gmail.com>
2022-02-04 13:29:13 +0900

    添加项目文件。

---
+++ FfxivAlarmClock/MainViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+
+namespace FfxivAlarmClock
+{
+    internal class MainViewModel : Prism.Mvvm.BindableBase
+    {
+        private static MainViewModel instance;
+        public static MainViewModel Instance => instance ??= new MainViewModel();
+
+        #region 内部控制用
+        internal Microsoft.UI.Dispatching.DispatcherQueue Dispatcher { private get; set; }
+        public DateTimeOffset EorzeaTime { get; private set; }
+        #endregion
+
+        #region 显示信息用
+        public string EorzeaTimeExp => EorzeaTime.ToString("HH:mm:ss");
+        public string LocalTime => DateTime.Now.ToString("t");
+        public string LocalDate => DateTime.Now.ToString("d");
+        #endregion
+
+        private MainViewModel() 
+        {
+            Task.Run(timer);
+        }
+
+        /// <summary>
+        /// 事件处理用计时器
+        /// </summary>
+        private void timer() 
+        {
+            while (true)
+            {
+                EorzeaTime = GetEorzeaTime();
+
+                if (Dispatcher != null)
+                {
+                    Dispatcher.TryEnqueue(() =>
+                    {
+                        RaisePropertyChanged(nameof(EorzeaTimeExp));
+                        RaisePropertyChanged(nameof(LocalTime));
+                    });
+                }
+                Task.Delay(1000).Wait();
+            }
+        }
+
+        /// <summary>
+        /// 计算艾欧泽亚时间
+        /// </summary>
+        /// <returns></returns>
+        private DateTimeOffset GetEorzeaTime() =>
+            DateTimeOffset.FromUnixTimeSeconds(DateTimeOffset.UtcNow.ToUnixTimeSeconds() * 3600 / 175);
+    }
+}
